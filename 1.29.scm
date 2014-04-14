efed9b093c2c4039457bbb291295f5014897569e
Yuri Feldman <arrayjam@tbqh.net>
2014-04-14 23:31:48 +1000

    Mon 14 Apr 2014 23:31:48 EST

--- 1.29.scm
+++ 1.29.scm
@@ -3,6 +3,13 @@
 
 (define (inc x) (+ x 1))
 
+(define (sum-iter term a next b)
+  (define (iter a result)
+    (if (> a b)
+	result
+	(iter (next a) (+ result (term a)))))
+  (iter 0 0))
+
 (define (sum term a next b)
   (if (> a b)
       0
@@ -16,30 +23,37 @@
 
 (define (integral f a b dx)
   (define (add-dx x) (+ x dx))
-  (* (sum f (+ a (/ dx 2.0)) add-dx b)
+  (* (sum-iter f (+ a (/ dx 2.0)) add-dx b)
      dx))
 
-(integral cube 0 1 0.01)
+(integral cube 0 2.3 0.01)
 
-(integral cube 0 1 0.001)
+(integral cube 0 2.3 0.001)
 
-(integral cube 0 1 0.0001)
+(integral cube 0 2.3 0.0001)
 
 
 ; h/3 * 
-
-
+		  
 ; go to n
 ; h = (b-a)/n
 ; at each k of n do f(a + kh)
 ; 0y0 + 4y1 + 2y2 + 4y3+y4 if n = 4 (n is always even)
 
-(define (simpsons f a b n k)
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a)
+		(accumulate combiner null-value term (next a) next b))))
+
+(define (a-sum term a next b) (accumulate + 0 term a next b))
+(define (a-product term a next b) (accumulate * 1 term a next b))
+
+
+(define (simpsons f a b n)
   (define h (/ (- b a) n))
-  (if (> k n)
-      0
-      (+ (* (coeff k n) (f (+ a (* k h))))
-	 (simpsons f a b n (+ 1 k)))))
+  (define (term k) (* (coeff k n) (f (+ a (* k h)))))
+  (/ (* h (a-sum term 0 inc n)) 3))
 
 ; Defines the coefficient used in the simpson's rule
 (define (coeff k n)
@@ -48,11 +62,27 @@
 	((= (modulo k 2) 1) 4)
 	((= (modulo k 2) 0) 2)))
 
-(define (s f a b n)
-  (define h (/ (- b a) n))
-  (/ (* h (simpsons f a b n 0)) 3))
+(simpsons cube 0 2.3 6) 
 
-(s cube 0 1 1000.0) 
+(define (product term a next b)
+  (if (> a b)
+      1
+      (* (term a)
+	 (product term (next a) next b))))
 
+(define (identity x) x)
 
+(define (wallis-pi n)
+  (define (next-even x) (if (even? x) x (+ 1 x)))
+  (define (next-odd x) (if (odd? x) x (+ 1 x)))
+  (define (num-f x) (next-even (+ 2 x)))
+  (define (den-f x) (next-odd (+ 2 x)))
+  (* 4.0 (/ (a-product num-f 0 inc n) (a-product den-f 0 inc n))))
+
+(wallis-pi 1)
+(wallis-pi 2)
+(wallis-pi 3)
+(wallis-pi 4)
+(wallis-pi 5)
+(wallis-pi 1000)
 
---
+++ 1.3.3.scm
@@ -0,0 +1,92 @@
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (search f neg-point pos-point)
+  (let ((midpoint (average neg-point pos-point)))
+    (if (close-enough? neg-point pos-point)
+	midpoint
+	(let ((test-value (f midpoint)))
+	  (cond ((positive? test-value)
+		 (search f neg-point midpoint))
+		((negative? test-value)
+		 (search f midpoint pos-point))
+		(else midpoint))))))
+
+(define (close-enough? x y)
+  (< (abs (- x y)) 0.001))
+
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+	(b-value (f b)))
+    (cond ((and (negative? a-value) (positive? b-value))
+	   (search f a b))
+	  ((and (negative? b-value) (positive? a-value))
+	   (search f b a))
+	  (else
+	   (error "Values are not of opposite sign" a b)))))
+
+(half-interval-method sin 2.0 4.0)
+
+(half-interval-method (lambda (x) (- (* x x x) (* 2 x) 3))
+		      1.0
+		      2.0)
+
+
+(define tolerance 0.000001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (newline)
+      (display guess)
+      (if (close-enough? guess next)
+	  next
+	  (try next))))
+  (try first-guess))
+
+(fixed-point cos 1.0)
+
+(fixed-point (lambda (y) (+ (sin y) (cos y)))
+	     1.0)
+
+(define (sqrt x)
+  (fixed-point (lambda (y) (average y (/ x y)))
+	       1.0))
+
+(sqrt 2)
+
+; 1.35
+(fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0)
+
+; 1.36
+(fixed-point (lambda (x) (/ (log 1000) (log x)))
+	     2.0)
+
+(fixed-point (lambda (x) (average x (/ (log 1000) (log x))))
+	     2.0)
+
+; 1.37
+(define (cont-frac-rec n d k)
+  (define (cont-frac n d k i)
+    (if (= k i)
+	0
+	(/ (n i) (+ (d i) (cont-frac n d k (+ 1 i))))))
+  (cont-frac n d k 0))
+
+(define (cont-frac-iter n d k)
+  (define (cont-frac n d k i result)
+    (if (= i k)
+	result
+	(cont-frac n d k (+ 1 i) (/ (n i) (+ (d i) result)))))
+  (cont-frac n d k 0 0))
+
+; 1/phi = 0.61803398875
+(cont-frac-rec (lambda (i) 1.0)
+	   (lambda (i) 1.0)
+	   11)
+
+(cont-frac-iter (lambda (i) 1.0)
+	   (lambda (i) 1.0)
+	   11)
